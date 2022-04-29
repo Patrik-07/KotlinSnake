1d90d290d19feafe15ce3f25c198132845bd94cc
Patrik-07 <patrik.santa07@gmail.com>
2022-04-30 01:56:55 +0200

    todo, remove grass types from gameObjects

--- src/main/kotlin/patrik07/snake/model/game/gameobjects/GrassLight.kt
+++ src/main/kotlin/patrik07/snake/model/game/gameobjects/GrassLight.kt
@@ -3,6 +3,8 @@ package patrik07.snake.model.game.gameobjects
 import patrik07.snake.model.game.GameObject
 import patrik07.snake.model.game.snake.Snake
 
+// TODO should remove grass types from gameObjectss
+
 class GrassLight : GameObject() {
     override fun collideWithSnake(snake: Snake) {}
 }
